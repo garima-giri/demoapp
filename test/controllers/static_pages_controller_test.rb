f03e09f85b8479da65084b47ac1001a5b039093d
garima_giri <garima_giri@berkeley.edu>
2015-12-02 00:25:51 +0000

    Created users

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "hi", "DemoApp"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "hi", "Help"
   end
 
 end
